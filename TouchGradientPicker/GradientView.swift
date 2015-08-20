2e6161ff9573f30b0854c73db49afce327cf03b0
mmertsock <mike.mertsock@gmail.com>
2015-08-19 21:30:17 -0400

    Initial project creation

---
+++ TouchGradientPicker/GradientView.swift
@@ -0,0 +1,9 @@
+//
+//  GradientView.swift
+//  TouchGradientPicker
+//
+//  Created by Mike Mertsock on 8/19/15.
+//  Copyright (c) 2015 Esker Apps. All rights reserved.
+//
+
+import UIKit
---
+++ TouchGradientPicker/TouchGradientPicker.h
@@ -0,0 +1,19 @@
+//
+//  TouchGradientPicker.h
+//  TouchGradientPicker
+//
+//  Created by Mike Mertsock on 8/19/15.
+//  Copyright (c) 2015 Esker Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TouchGradientPicker.
+FOUNDATION_EXPORT double TouchGradientPickerVersionNumber;
+
+//! Project version string for TouchGradientPicker.
+FOUNDATION_EXPORT const unsigned char TouchGradientPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TouchGradientPicker/PublicHeader.h>
+
+
